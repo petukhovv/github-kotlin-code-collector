0212bc7978b1c68e1f9a8b1973e17b6c9c796d51
Petukhov Victor <PetukhovVictor@users.noreply.github.com>
2017-11-21 14:19:56 +0300

    --no-compile to --no-parse renamed

--- code_search.py
+++ code_search.py
@@ -6,18 +6,18 @@ def code_file_handler(code, config):
     """
     Collection of files with Kotlin code from Github, and:
         1) either their parsing and resulting AST conversion to JSON;
-        2) or just saving in .kt file (if --no-compile option is specified).
+        2) or just saving in .kt file (if --no-parse option is specified).
     """
-    getting_content_time = code.write_file(None if config['no_compile'] else config['kt_code_temp_file'])
+    getting_content_time = code.write_file(None if config['no_parse'] else config['kt_code_temp_file'])
     if getting_content_time is None:
         return
-    if not config['no_compile']:
+    if not config['no_parse']:
         compilation_time = code.compile(config['compiler_path'])
         code.remove_file()
         transformation_time = AstHelper.text2json(code)
 
     times = [getting_content_time]
-    if not config['no_compile']:
+    if not config['no_parse']:
         times.append(transformation_time, compilation_time)
     prefix = str(code.number) + '|' + code.obj.html_url + '|' + str(code.obj.size)
     TimeLogger.console_output(times, prefix=prefix)
